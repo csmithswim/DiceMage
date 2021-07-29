5983a43740dda68427326991cabae42a90aad5ac
Cullen S <cullensmi@gmail.com>
2021-07-29 18:58:05 -0400

    added some more methods

--- src/com/csmithswim/Main.java
+++ src/com/csmithswim/Main.java
@@ -1,8 +1,22 @@
 package com.csmithswim;
 
 public class Main {
+    //Monster is a single die limited by sides.
+
+    //Every turn gets 1 mana
+    //Generate mana, # of dice - 2
+    //Create a monster, roll a single monster or multiple monsters
+
 
     public static void main(String[] args) {
-	// write your code here
+        Deck playerOneDeck = new Deck();
+        Deck playerTwoDeck = new Deck();
+
+        playerOneDeck.roll();
+        playerTwoDeck.roll();
+
+        System.out.println(playerOneDeck.displayDeck());
+        System.out.println(playerTwoDeck.displayDeck());
+
     }
 }
